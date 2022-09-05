97f4f2d95a6573a6d23c12eabe30145b0eec0404
Yahor Vasilenko <yahor.vasilenko@itechart-group.com>
2022-09-05 23:24:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  const [size, setSize] = useState(8);
+  const [count, setCount] = useState(10);
+  return (<>
+    <label>Size per row:</label><input value={size} onChange={(e) => setSize(+e.target.value)} type="number" step="1" />
+    <br/>
+    <br/>
+    <br/>
+    <label>Count:</label><input value={count} onChange={(e) => setCount(+e.target.value)} type="number" step="1" />
+    <br/>
+    <br/>
+    <br/>
+    <div className={`container container--${size}`}>
+      {new Array(count).fill(0).map((item,i) => <div className="item" key={i}>{i + 1}</div>)}
+    </div>
+    </>
+  );
+}
+
+export default App;
